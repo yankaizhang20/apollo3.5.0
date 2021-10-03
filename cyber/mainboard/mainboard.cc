9205a0f864ec65d634cf72615ea43b17c903d7ef
yankaizhang20 <yankaizhang2020@163.com>
2021-10-03 17:38:05 +0800

    Planning模块启动过程

--- cyber/mainboard/mainboard.cc
+++ cyber/mainboard/mainboard.cc
@@ -20,33 +20,32 @@
 #include "cyber/mainboard/module_argument.h"
 #include "cyber/mainboard/module_controller.h"
 #include "cyber/state.h"
-
 #include "gflags/gflags.h"
 
 using apollo::cyber::mainboard::ModuleArgument;
 using apollo::cyber::mainboard::ModuleController;
 
 int main(int argc, char** argv) {
-  google::SetUsageMessage("we use this program to load dag and run user apps.");
+        google::SetUsageMessage("we use this program to load dag and run user apps.");
 
-  // parse the argument
-  ModuleArgument module_args;
-  module_args.ParseArgument(argc, argv);
+        // parse the argument
+        ModuleArgument module_args;
+        module_args.ParseArgument(argc, argv);
 
-  // initialize cyber
-  apollo::cyber::Init(argv[0]);
+        // initialize cyber
+        apollo::cyber::Init(argv[0]);
 
-  // start module
-  ModuleController controller(module_args);
-  if (!controller.Init()) {
-    controller.Clear();
-    AERROR << "module start error.";
-    return -1;
-  }
+        // start module
+        ModuleController controller(module_args);
+        if (!controller.Init()) {
+                controller.Clear();
+                AERROR << "module start error.";
+                return -1;
+        }
 
-  apollo::cyber::WaitForShutdown();
-  controller.Clear();
-  AINFO << "exit mainboard.";
+        apollo::cyber::WaitForShutdown();
+        controller.Clear();
+        AINFO << "exit mainboard.";
 
-  return 0;
+        return 0;
 }
--- modules/dreamview/backend/hmi/hmi_worker.h
+++ modules/dreamview/backend/hmi/hmi_worker.h
@@ -22,7 +22,6 @@
 
 #include "boost/thread/locks.hpp"
 #include "boost/thread/shared_mutex.hpp"
-
 #include "cyber/cyber.h"
 #include "modules/canbus/proto/chassis.pb.h"
 #include "modules/common/proto/drive_event.pb.h"
@@ -40,78 +39,76 @@ namespace dreamview {
 
 // Singleton worker which does the actual work of HMI actions.
 class HMIWorker {
- public:
-  HMIWorker() : HMIWorker(cyber::CreateNode("HMI")) {}
-  explicit HMIWorker(const std::shared_ptr<apollo::cyber::Node>& node);
-  void Start();
-  void Stop();
+    public:
+        HMIWorker() : HMIWorker(cyber::CreateNode("HMI")) {}
+        explicit HMIWorker(const std::shared_ptr<apollo::cyber::Node>& node);
+        void Start();
+        void Stop();
 
-  // HMI action trigger.
-  bool Trigger(const HMIAction action);
-  bool Trigger(const HMIAction action, const std::string& value);
+        // HMI action trigger.
+        bool Trigger(const HMIAction action);
+        bool Trigger(const HMIAction action, const std::string& value);
 
-  // Register handler which will be called on HMIStatus update.
-  // It will be called ASAP if there are changes, or else periodically
-  // controlled by FLAGS_hmi_status_update_interval.
-  using StatusUpdateHandler =
-      std::function<void(const bool status_changed, HMIStatus* status)>;
-  inline void RegisterStatusUpdateHandler(StatusUpdateHandler handler) {
-    status_update_handlers_.push_back(handler);
-  }
+        // Register handler which will be called on HMIStatus update.
+        // It will be called ASAP if there are changes, or else periodically
+        // controlled by FLAGS_hmi_status_update_interval.
+        using StatusUpdateHandler = std::function<void(const bool status_changed, HMIStatus* status)>;
+        inline void RegisterStatusUpdateHandler(StatusUpdateHandler handler) {
+                status_update_handlers_.push_back(handler);
+        }
 
-  // Submit a DriveEvent.
-  void SubmitDriveEvent(const uint64_t event_time_ms,
-                        const std::string& event_msg,
-                        const std::vector<std::string>& event_types);
+        // Submit a DriveEvent.
+        void SubmitDriveEvent(const uint64_t event_time_ms, const std::string& event_msg,
+                              const std::vector<std::string>& event_types);
 
-  // Get current HMI status.
-  HMIStatus GetStatus() const;
+        // Get current HMI status.
+        HMIStatus GetStatus() const;
 
-  // Load HMIConfig and HMIMode.
-  static HMIConfig LoadConfig();
-  static HMIMode LoadMode(const std::string& mode_config_path);
+        // Load HMIConfig and HMIMode.
+        static HMIConfig LoadConfig();
+        static HMIMode LoadMode(const std::string& mode_config_path);
 
- private:
-  void InitReadersAndWriters();
-  void InitStatus();
-  void StatusUpdateThreadLoop();
+    private:
+        void InitReadersAndWriters();
+        void InitStatus();
+        void StatusUpdateThreadLoop();
 
-  // Start / reset current mode.
-  void SetupMode() const;
-  void ResetMode() const;
+        // Start / reset current mode.
+        void SetupMode() const;
+        void ResetMode() const;
 
-  // Change current mode, launch, map, vehicle and driving mode.
-  void ChangeMode(const std::string& mode_name);
-  void ChangeMap(const std::string& map_name);
-  void ChangeVehicle(const std::string& vehicle_name);
-  bool ChangeDrivingMode(const apollo::canbus::Chassis::DrivingMode mode);
+        // Change current mode, launch, map, vehicle and driving mode.
+        void ChangeMode(const std::string& mode_name);
+        void ChangeMap(const std::string& map_name);
+        void ChangeVehicle(const std::string& vehicle_name);
+        bool ChangeDrivingMode(const apollo::canbus::Chassis::DrivingMode mode);
 
-  // Start / stop a module.
-  void StartModule(const std::string& module) const;
-  void StopModule(const std::string& module) const;
+        // Start / stop a module.
+        void StartModule(const std::string& module) const;
+        void StopModule(const std::string& module) const;
 
-  void RecordAudio(const std::string& data);
+        void RecordAudio(const std::string& data);
+        //@zyk:config_存放着配置文件名和配置文件路径的映射
+        const HMIConfig config_;
 
-  const HMIConfig config_;
+        // HMI status maintainence.
+        HMIStatus status_;
+        //@zyk:current_mode_存放当前模式的各种配置项
+        HMIMode current_mode_;
+        bool status_changed_ = false;
+        bool stop_ = false;
+        mutable boost::shared_mutex status_mutex_;
+        std::future<void> thread_future_;
+        std::vector<StatusUpdateHandler> status_update_handlers_;
 
-  // HMI status maintainence.
-  HMIStatus status_;
-  HMIMode current_mode_;
-  bool status_changed_ = false;
-  bool stop_ = false;
-  mutable boost::shared_mutex status_mutex_;
-  std::future<void> thread_future_;
-  std::vector<StatusUpdateHandler> status_update_handlers_;
-
-  // Cyber members.
-  std::shared_ptr<apollo::cyber::Node> node_;
-  std::shared_ptr<cyber::Reader<apollo::canbus::Chassis>> chassis_reader_;
-  std::shared_ptr<cyber::Writer<HMIStatus>> status_writer_;
-  std::shared_ptr<cyber::Writer<AudioCapture>> audio_capture_writer_;
-  std::shared_ptr<cyber::Writer<apollo::control::PadMessage>> pad_writer_;
-  std::shared_ptr<cyber::Writer<apollo::common::DriveEvent>>
-      drive_event_writer_;
+        // Cyber members.
+        std::shared_ptr<apollo::cyber::Node> node_;
+        std::shared_ptr<cyber::Reader<apollo::canbus::Chassis>> chassis_reader_;
+        std::shared_ptr<cyber::Writer<HMIStatus>> status_writer_;
+        std::shared_ptr<cyber::Writer<AudioCapture>> audio_capture_writer_;
+        std::shared_ptr<cyber::Writer<apollo::control::PadMessage>> pad_writer_;
+        std::shared_ptr<cyber::Writer<apollo::common::DriveEvent>> drive_event_writer_;
 };
 
-}  // namespace dreamview
-}  // namespace apollo
+} // namespace dreamview
+} // namespace apollo
