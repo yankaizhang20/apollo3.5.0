cadb7e2a29b9dbd793ac04c79e0203b665868e9a
yankaizhang20 <yankaizhang2020@163.com>
2021-10-09 21:41:11 +0800

    单例模式

--- cyber/common/macros.h
+++ cyber/common/macros.h
@@ -29,13 +29,12 @@ DEFINE_TYPE_TRAIT(HasShutdown, Shutdown)
 
 template <typename T>
 typename std::enable_if<HasShutdown<T>::value>::type CallShutdown(T *instance) {
-  instance->Shutdown();
+        instance->Shutdown();
 }
 
 template <typename T>
-typename std::enable_if<!HasShutdown<T>::value>::type CallShutdown(
-    T *instance) {
-  (void)instance;
+typename std::enable_if<!HasShutdown<T>::value>::type CallShutdown(T *instance) {
+        (void)instance;
 }
 
 // There must be many copy-paste versions of these macros which are same
@@ -45,31 +44,40 @@ typename std::enable_if<!HasShutdown<T>::value>::type CallShutdown(
 
 #define UNUSED(param) (void)param
 
-#define DISALLOW_COPY_AND_ASSIGN(classname) \
-  classname(const classname &) = delete;    \
-  classname &operator=(const classname &) = delete;
+#define DISALLOW_COPY_AND_ASSIGN(classname)    \
+        classname(const classname &) = delete; \
+        classname &operator=(const classname &) = delete;
+//@zyk:std::once_flag用于std::call_once
+//@zyk:std::call_once用于在多线程时保证函数只被调用一次
+//@zyk:std::nothrow用于使new失败时返回空指针
+/*@zyk:
+单例模式注意
+1.构造函数，拷贝构造函数，赋值号，析构函数private
+2.有一个公有接口用于创建或者获得该类的实例，由于无法通过构造函数显示创建实例，所以该类应当是静态的
+3.实例要一直存在，所以实例的指针应当是静态的，以便下次调用接口时可以访问到该实例
+4.多线程安全，用std::call_once
+5.记得写清理函数
+*/
+#define DECLARE_SINGLETON(classname)                                                              \
+    public:                                                                                       \
+        static classname *Instance(bool create_if_needed = true) {                                \
+                static classname *instance = nullptr;                                             \
+                if (!instance && create_if_needed) {                                              \
+                        static std::once_flag flag;                                               \
+                        std::call_once(flag, [&] { instance = new (std::nothrow) classname(); }); \
+                }                                                                                 \
+                return instance;                                                                  \
+        }                                                                                         \
+                                                                                                  \
+        static void CleanUp() {                                                                   \
+                auto instance = Instance(false);                                                  \
+                if (instance != nullptr) {                                                        \
+                        CallShutdown(instance);                                                   \
+                }                                                                                 \
+        }                                                                                         \
+                                                                                                  \
+    private:                                                                                      \
+        classname();                                                                              \
+        DISALLOW_COPY_AND_ASSIGN(classname)
 
-#define DECLARE_SINGLETON(classname)                                      \
- public:                                                                  \
-  static classname *Instance(bool create_if_needed = true) {              \
-    static classname *instance = nullptr;                                 \
-    if (!instance && create_if_needed) {                                  \
-      static std::once_flag flag;                                         \
-      std::call_once(flag,                                                \
-                     [&] { instance = new (std::nothrow) classname(); }); \
-    }                                                                     \
-    return instance;                                                      \
-  }                                                                       \
-                                                                          \
-  static void CleanUp() {                                                 \
-    auto instance = Instance(false);                                      \
-    if (instance != nullptr) {                                            \
-      CallShutdown(instance);                                             \
-    }                                                                     \
-  }                                                                       \
-                                                                          \
- private:                                                                 \
-  classname();                                                            \
-  DISALLOW_COPY_AND_ASSIGN(classname)
-
-#endif  // CYBER_COMMON_MACROS_H_
+#endif // CYBER_COMMON_MACROS_H_
