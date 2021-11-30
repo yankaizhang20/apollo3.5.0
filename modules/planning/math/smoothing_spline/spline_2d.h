1e890a0edac0f74430931ad2036d5cabc34eb56e
yankaizhang20 <yankaizhang2020@163.com>
2021-11-30 17:14:08 +0800

    格式

--- modules/planning/math/smoothing_spline/spline_2d.h
+++ modules/planning/math/smoothing_spline/spline_2d.h
@@ -25,7 +25,6 @@
 #include <vector>
 
 #include "Eigen/Core"
-
 #include "modules/planning/math/polynomial_xd.h"
 #include "modules/planning/math/smoothing_spline/spline_2d_seg.h"
 
@@ -33,30 +32,30 @@ namespace apollo {
 namespace planning {
 
 class Spline2d {
- public:
-  Spline2d(const std::vector<double>& t_knots, const uint32_t order);
-  std::pair<double, double> operator()(const double t) const;
-  double x(const double t) const;
-  double y(const double t) const;
-  double DerivativeX(const double t) const;
-  double DerivativeY(const double t) const;
-  double SecondDerivativeX(const double t) const;
-  double SecondDerivativeY(const double t) const;
-  double ThirdDerivativeX(const double t) const;
-  double ThirdDerivativeY(const double t) const;
-  bool set_splines(const Eigen::MatrixXd& params, const uint32_t order);
-  const Spline2dSeg& smoothing_spline(const uint32_t index) const;
-  const std::vector<double>& t_knots() const;
-  uint32_t spline_order() const;
+    public:
+        Spline2d(const std::vector<double>& t_knots, const uint32_t order);
+        std::pair<double, double> operator()(const double t) const;
+        double x(const double t) const;
+        double y(const double t) const;
+        double DerivativeX(const double t) const;
+        double DerivativeY(const double t) const;
+        double SecondDerivativeX(const double t) const;
+        double SecondDerivativeY(const double t) const;
+        double ThirdDerivativeX(const double t) const;
+        double ThirdDerivativeY(const double t) const;
+        bool set_splines(const Eigen::MatrixXd& params, const uint32_t order);
+        const Spline2dSeg& smoothing_spline(const uint32_t index) const;
+        const std::vector<double>& t_knots() const;
+        uint32_t spline_order() const;
 
- private:
-  uint32_t find_index(const double x) const;
+    private:
+        uint32_t find_index(const double x) const;
 
- private:
-  std::vector<Spline2dSeg> splines_;
-  std::vector<double> t_knots_;
-  uint32_t spline_order_;
+    private:
+        std::vector<Spline2dSeg> splines_;
+        std::vector<double> t_knots_;
+        uint32_t spline_order_;
 };
 
-}  // namespace planning
-}  // namespace apollo
+} // namespace planning
+} // namespace apollo
