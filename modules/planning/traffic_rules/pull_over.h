92702a46133213c74108bd086c80c7e3c44d6974
yankaizhang20 <yankaizhang2020@163.com>
2022-04-03 21:07:10 +0800

    交通规则-靠边停车pullover

--- modules/planning/traffic_rules/pull_over.h
+++ modules/planning/traffic_rules/pull_over.h
@@ -38,82 +38,79 @@ namespace planning {
  */
 
 class PullOver : public TrafficRule {
- public:
-  explicit PullOver(const TrafficRuleConfig& config);
-  virtual ~PullOver() = default;
+    public:
+        explicit PullOver(const TrafficRuleConfig& config);
+        virtual ~PullOver() = default;
 
-  common::Status ApplyRule(Frame* const frame,
-                           ReferenceLineInfo* const reference_line_info);
+        common::Status ApplyRule(Frame* const frame, ReferenceLineInfo* const reference_line_info);
 
- private:
-  enum ValidateStopPointCode {
-    OK = 0,
-    OUT_OF_REFERENCE_LINE = 1,
-    PASS_DEST_POINT_TOO_FAR = 2,
-    BEHIND_ADC = 3,
-    OPERATION_LENGTH_NOT_ENOUGH = 4,
-    PARKING_SPOT_NOT_AVAIL = 5,
-  };
+    private:
+        enum ValidateStopPointCode {
+                OK = 0,
+                OUT_OF_REFERENCE_LINE = 1,
+                PASS_DEST_POINT_TOO_FAR = 2,
+                BEHIND_ADC = 3,
+                OPERATION_LENGTH_NOT_ENOUGH = 4,
+                PARKING_SPOT_NOT_AVAIL = 5,
+        };
 
-  /**
-   * Check if the planning status is in pull over mode
-   */
-  bool IsPullOver() const;
+        /**
+         * Check if the planning status is in pull over mode
+         */
+        bool IsPullOver() const;
 
-  /**
-   * get a pull over stop point
-   */
-  int GetPullOverStopPoint(common::PointENU* stop_point);
+        /**
+         * get a pull over stop point
+         */
+        int GetPullOverStopPoint(common::PointENU* stop_point);
 
-  /**
-   * check if on a crosswalk/junction/clear_area/speedbumper/etc overlap
-   */
-  bool OnOverlap(const double start_s, const double end_s);
+        /**
+         * check if on a crosswalk/junction/clear_area/speedbumper/etc overlap
+         */
+        bool OnOverlap(const double start_s, const double end_s);
 
-  /**
-   * Find a safe place to pull over based on the vehicle's current state.
-   */
-  int FindPullOverStop(const double stop_point_s, common::PointENU* stop_point);
-  int FindPullOverStop(common::PointENU* stop_point);
+        /**
+         * Find a safe place to pull over based on the vehicle's current state.
+         */
+        int FindPullOverStop(const double stop_point_s, common::PointENU* stop_point);
+        int FindPullOverStop(common::PointENU* stop_point);
 
-  /**
-   * Check if a stop point is valid based on current vehicle status
-   * The stop point could be invalid if it is occupied by other obstacles;
-   * The stop point could be invalid if the vehicle has passed this point
-   */
-  ValidateStopPointCode IsValidStop(const common::PointENU& stop_point) const;
-  ValidateStopPointCode IsValidStop(const common::SLPoint& stop_point_sl) const;
+        /**
+         * Check if a stop point is valid based on current vehicle status
+         * The stop point could be invalid if it is occupied by other obstacles;
+         * The stop point could be invalid if the vehicle has passed this point
+         */
+        ValidateStopPointCode IsValidStop(const common::PointENU& stop_point) const;
+        ValidateStopPointCode IsValidStop(const common::SLPoint& stop_point_sl) const;
 
-  /**
-   * check if pull-over is completed
-   */
-  bool CheckPullOverComplete();
+        /**
+         * check if pull-over is completed
+         */
+        bool CheckPullOverComplete();
 
-  /**
-   * build stop deceleration
-   */
-  bool CheckStopDeceleration(const double stop_line_s) const;
+        /**
+         * build stop deceleration
+         */
+        bool CheckStopDeceleration(const double stop_line_s) const;
 
-  /**
-   * build stop decision
-   */
-  int BuildPullOverStop(const common::PointENU& stop_point);
-  int BuildInLaneStop(const common::PointENU& pull_over_stop_point);
-  int BuildStopDecision(const std::string& vistual_obstacle_id_postfix,
-                        const double stop_line_s,
-                        const common::PointENU& stop_point,
-                        const double stop_point_heading);
+        /**
+         * build stop decision
+         */
+        int BuildPullOverStop(const common::PointENU& stop_point);
+        int BuildInLaneStop(const common::PointENU& pull_over_stop_point);
+        int BuildStopDecision(const std::string& vistual_obstacle_id_postfix, const double stop_line_s,
+                              const common::PointENU& stop_point, const double stop_point_heading);
 
- private:
-  static constexpr char const* const PULL_OVER_VO_ID_PREFIX = "PO_";
-  static constexpr char const* const INLANE_STOP_VO_ID_POSTFIX = "_INLANE";
-  static constexpr double PARKING_SPOT_LONGITUDINAL_BUFFER = 1.0;
-  static uint32_t failure_count_;
-  static common::PointENU stop_point_;
-  static common::PointENU inlane_stop_point_;
-  Frame* frame_ = nullptr;
-  ReferenceLineInfo* reference_line_info_ = nullptr;
+    private:
+        static constexpr char const* const PULL_OVER_VO_ID_PREFIX = "PO_";
+        static constexpr char const* const INLANE_STOP_VO_ID_POSTFIX = "_INLANE";
+        static constexpr double PARKING_SPOT_LONGITUDINAL_BUFFER = 1.0;
+        static uint32_t failure_count_;
+        static common::PointENU stop_point_;
+        static common::PointENU inlane_stop_point_;
+        Frame* frame_ = nullptr;
+        ReferenceLineInfo* reference_line_info_ = nullptr;
 };
 
-}  // namespace planning
-}  // namespace apollo
+} // namespace planning
+} // namespace apollo
